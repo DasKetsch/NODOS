6835386110ee9dd1bb45b79470400fecc973df16
Ketsch <87279547+DasKetsch@users.noreply.github.com>
2023-10-30 16:31:31 +0500

    Add files via upload

--- kernel.h
+++ kernel.h
@@ -1,6 +1,22 @@
-#ifndef KERNEL_H
-#define KERNEL_H
 
-extern  void kpanic(struct regs *r);
-
-#endif
+void printch(char i,int adr){
+ *(char*)adr = i;
+ return;
+}
+int adr;
+int k;
+void print(char* i){
+ if (adr < 0xb8000){
+  adr = 0xb8000;
+}
+ for (int j = 0; i[j] != '\0'; j++){
+  k++;
+  printch(i[j],adr);
+  adr = adr + 2;
+  if(i[j] == '\n'){
+   adr = adr - ((k)*2);
+   adr = adr + 160;
+   k = 0;
+  }
+ }
+}
